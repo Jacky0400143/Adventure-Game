e5f769362844a03cbfce99b0faa060c48c54cc65
Jacky0400143 <Wang@WANG-NB-SONY-S13P.r1802.tciii.org.tw>
2023-07-10 16:02:56 +0800

    20230710_2

--- JWJAVA/src/me/synology/zankyonokaze/ADV_Beta_002.java
+++ JWJAVA/src/me/synology/zankyonokaze/ADV_Beta_002.java
@@ -4,15 +4,23 @@ package me.synology.zankyonokaze;
 	import java.awt.color.*; //導入java.awt.color包，此包含有所有顏色空間和色彩型別相關的類別。
 	import java.awt.event.ActionEvent;
 	import java.awt.event.ActionListener;
+	import java.awt.BorderLayout;
 	import java.awt.Color;   //導入java.awt.Color類別，該類別用於封裝顏色的RGB值。
 	import java.awt.Container; //導入java.awt.Container類別，該類別是Swing組件的容器，用於保存和管理其他組件。
 	import java.awt.Font;
-
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+	import java.awt.event.MouseEvent;
+	
 	import javax.swing.JFrame; //導入javax.swing.JFrame類別，此類別代表一個應用程式視窗，用於添加其他GUI組件。
 	import javax.swing.JPanel;
 	import javax.swing.JLabel;
 	import javax.swing.JButton;
 	import javax.swing.JTextArea;
+	import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+	
+
 	
 
 	public class ADV_Beta_002 { // 定義一個名為ADV_Beta_001的public class。
@@ -20,19 +28,27 @@ package me.synology.zankyonokaze;
 		JFrame GameWindow; // 宣告一個JFrame物件名稱為GameWindow，這將是遊戲的主視窗。
 		Container GameContainer; // 宣告一個Container物件名稱為GameContainer，這將是主視窗中的容器，用於保存和管理其他GUI組件。
 		JPanel TitleNamePanel, StartGButtonPanel, LoadGButtonPanel, ScoreLButtonPanel, ExtraButtonPanel,
-				MainStoryPanel, ChoiceButtonPanel;
-		JLabel TitleNameLabel;
+				MainStoryPanel, ChoiceButtonPanel ,PrologueDialoguePanel ,PlayerPanel;
+		JLabel TitleNameLabel, PrologueDialogue;
 		Font TitleFont = new Font("Ink Free", Font.BOLD, 100);	//開始標題的字體
 		Font NormalBFont = new Font("微軟正黑體", Font.PLAIN, 24);	//開始按鈕的字體
-		JButton StartGameButton, LoadGameButton, ScoreListButton, ExtraButton;
+		JButton StartGameButton, LoadGameButton, ScoreListButton, ExtraButton, NextButton,
+				Choice1, Choice2, Choice3, Choice4;
 		JTextArea MainStoryArea;
 		
 		TitleScreenProcess TSProcess = new TitleScreenProcess();
 		
+	    private int PrologueDialogueIndex = 0;
+	    private String[] PrologueDialogues = {
+	            "我好像睡著了...",
+	            "果然是太累了嗎...",
+	            "這裡是哪裡..."
+	    };
 		
 		public static void main(String[] args) { // Java主程式的進入點。
 			
 			new ADV_Beta_002(); // 建立ADV_Beta_001類別的一個新實例，這將自動調用下面的建構子並開啟視窗。
+//	        SwingUtilities.invokeLater(ADV_Beta_002::new);
 		}
 		
 		public ADV_Beta_002() { // ADV_Beta_001類別的建構子。
@@ -118,6 +134,39 @@ package me.synology.zankyonokaze;
 
 			
 		}
+
+		public void CreatStoryPrologueScreen() {	//這一段需要再研究原理，主要是一個序幕對話，透過滑鼠點擊
+			TitleNamePanel.setVisible(false);
+			StartGButtonPanel.setVisible(false);
+			LoadGButtonPanel.setVisible(false);
+			ScoreLButtonPanel.setVisible(false);
+			ExtraButtonPanel.setVisible(false);
+			
+
+		    PrologueDialoguePanel = new JPanel();
+		    PrologueDialoguePanel.setBounds(140, 300, 1000, 150);
+		    PrologueDialoguePanel.setBackground(Color.black);
+		    PrologueDialoguePanel.setLayout(new BorderLayout());
+		    
+		    PrologueDialogue = new JLabel("", SwingConstants.CENTER);
+		    PrologueDialogue.setForeground(Color.green);
+		    PrologueDialogue.setFont(new Font("Serif", Font.PLAIN, 24));
+		    PrologueDialoguePanel.add(PrologueDialogue, BorderLayout.CENTER);
+
+		    PrologueDialoguePanel.addMouseListener(new MouseAdapter() {
+		        @Override
+		        public void mouseClicked(MouseEvent e) {
+		            if(PrologueDialogueIndex < PrologueDialogues.length) {
+		             PrologueDialogue.setText(PrologueDialogues[PrologueDialogueIndex++]);
+		            }
+		            else {
+		            	CreateGameScreen();  
+		            }
+		        }
+		    });
+		    GameWindow.add(PrologueDialoguePanel);
+		}
+		
 		
 		public void CreateGameScreen() {
 			
@@ -126,12 +175,13 @@ package me.synology.zankyonokaze;
 			LoadGButtonPanel.setVisible(false);
 			ScoreLButtonPanel.setVisible(false);
 			ExtraButtonPanel.setVisible(false);
+			PrologueDialoguePanel.setVisible(false);
+			
 			
 			MainStoryPanel = new JPanel();
-			MainStoryPanel.setBounds(140, 500, 1000, 150);
-			MainStoryPanel.setBackground(Color.blue);
+			MainStoryPanel.setBounds(140, 470, 1000, 150);
+			MainStoryPanel.setBackground(Color.gray);
 			GameContainer.add(MainStoryPanel);
-			
 			
 			MainStoryArea = new JTextArea("Test Main Story Area");
 			MainStoryArea.setBounds(140, 100, 1000, 250);
@@ -141,20 +191,57 @@ package me.synology.zankyonokaze;
 			MainStoryArea.setLineWrap(true);
 			MainStoryPanel.add(MainStoryArea);
 			
+			
 			ChoiceButtonPanel = new JPanel();
-			ChoiceButtonPanel.setBounds(350, 600, 500, 150);
-			ChoiceButtonPanel.setBackground(Color.red);
+			ChoiceButtonPanel.setBounds(310, 620, 650, 25);
+			ChoiceButtonPanel.setBackground(Color.black);
+			ChoiceButtonPanel.setLayout(new GridLayout(1, 4));
 			GameContainer.add(ChoiceButtonPanel);
 			
+			Choice1 = new JButton("Choice 1");
+			Choice1.setBackground(Color.black);
+			Choice1.setForeground(Color.white);
+			Choice1.setFont(NormalBFont);
+			ChoiceButtonPanel.add(Choice1);
+			
+			Choice2 = new JButton("Choice 2");
+			Choice2.setBackground(Color.black);
+			Choice2.setForeground(Color.white);
+			Choice2.setFont(NormalBFont);
+			ChoiceButtonPanel.add(Choice2);
+			
+			Choice3 = new JButton("Choice 3");
+			Choice3.setBackground(Color.black);
+			Choice3.setForeground(Color.white);
+			Choice3.setFont(NormalBFont);
+			ChoiceButtonPanel.add(Choice3);
+			
+			Choice4 = new JButton("Choice 4");
+			Choice4.setBackground(Color.black);
+			Choice4.setForeground(Color.white);
+			Choice4.setFont(NormalBFont);
+			ChoiceButtonPanel.add(Choice4);
+			
+			
+			PlayerPanel = new JPanel();
+			PlayerPanel.setBounds(140, 100, 500, 50);
 		}
+		
 		
 		public class TitleScreenProcess implements ActionListener {
 			
 			public void actionPerformed(ActionEvent event) {
 				
-				CreateGameScreen();
+				CreatStoryPrologueScreen();
 			}
 		}
 		
 		
+//		public class PrologueScreenProcess implements ActionListener {
+//			
+//			public void actionPerformed(ActionEvent event) {
+//				
+//				CreateGameScreen();
+//			}
+//		}
 	}
